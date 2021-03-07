f8b5e389ce612ad819ca4b72b81aa41cfc4dda55
karlaaraujo <karlachs.a@gmail.com>
2021-03-07 14:44:44 -0300

    modificação usando SESSION

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<?php 
+    session_start();    //inicia a sessão antes de qualquer outro script abaixo
+?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
@@ -9,12 +13,25 @@
 <body>
     <p>Formulário para inscrição de competidores</p>
 
-    <form action="script.php" method="post"> <!--o action é o responsável por identificar para qual endereço os dados devem ser submetidos para serem processados-->
+    <form action="script.php" method="post">    <!--o action é o responsável por identificar para qual endereço os dados devem ser submetidos para serem processados-->
+        
+        <?php 
+        $mensagemDeErro = isset($_SESSION["mensagem-de-erro"]) ? $_SESSION["mensagem-de-erro"] : "";    //SE $_SESSION tiver um valor na chave "mensagem-de-erro", armazene este valor em $mensagemDeErro. ELSE, armazene nd
+        if (!empty($mensagemDeErro)){
+            echo $mensagemDeErro;
+        }
+
+        $mensagemDeSucesso = isset($_SESSION["mensagem-de-sucesso"]) ? $_SESSION["mensagem-de-sucesso"] : "";    //SE $_SESSION tiver um valor na chave "mensagem-de-erro", armazene este valor em $mensagemDeErro. ELSE, armazene nd
+        if (!empty($mensagemDeSucesso)){
+            echo $mensagemDeSucesso;
+        }
+        ?>
 
         <p>Nome: <input type="text" name="nome"> </p>
         <p>Idade: <input type="text" name="idade"> </p>
 
         <button type="submit">Enviar</button>
+       
     </form>
 </body>
 </html>
--- script.php
+++ script.php
@@ -1,36 +1,47 @@
 <?php 
 
-$nome = $_POST['nome']; //método post recupera os dados de uma tag html com um name='nome'
+session_start();    //inicia uma sessão http nesse script
+
+$nome = $_POST['nome'];     //método post recupera os dados de uma tag html com um name='nome'
 $idade = $_POST['idade'];
 
 
-//validação dos dados
+//-------validação dos dados----------
 
-if(empty($nome)){ //se o nome estiver vazio
-    echo "O nome não pode ser vazio";
-    return; //este return faz com que toda linha de código que vem depois não seja executada
+if(empty($nome)){   //se o nome estiver vazio
+    $_SESSION["mensagem-de-erro"] = "O nome não pode ser vazio";    //inicia o array associativo $_SESSION e associa a chave "mensagem-de-erro" ao valor "o nome n pode ser vazio"
+    header("location: index.php");  //manipula o header da requisição no campo location para redirecionar para o index.php
+    return;     //este return faz com que toda linha de código que vem depois não seja executada
 }
-else if (strlen($nome) < 3){ //strlen retorna a qtd de caracteres da string
-    echo "O nome deve conter mais de 3 caracteres";
+else if (strlen($nome) < 3){    //strlen retorna a qtd de caracteres da string
+    $_SESSION["mensagem-de-erro"] = "O nome deve conter mais de 3 caracteres";    
+    header("location: index.php");
     return;
 }
 else if (strlen($nome)> 40){
-    echo "O nome não pode ultrapassar 40 caracteres";
+    $_SESSION["mensagem-de-erro"] = "O nome não pode ultrapassar 40 caracteres";    
+    header("location: index.php");
     return;
 }
 
-if(!is_numeric($idade)){ // se NÃO for uma string numérica
-    echo "A idade precisa ser um número";
+else if(!is_numeric($idade)){    // se NÃO for uma string numérica
+    $_SESSION["mensagem-de-erro"] = "A idade precisa ser um número";    //inicia o array associativo $_SESSION e associa a chave "mensagem-de-erro" ao valor "o nome n pode ser vazio"
+    header("location: index.php");
     return;
 }
 
+//--------diz categoria da idade---------
 
 if ($idade >= 6 && $idade<=12){
-    echo "O participante ".$nome." compete na categoria infantil";
+    $_SESSION["mensagem-de-sucesso"] = "O participante ".$nome." compete na categoria infantil";    
+    header("location: index.php");
 }
 else if ($idade >= 13 && $idade<=18){
-    echo "O participante ".$nome." compete na categoria adolescente";
+    $_SESSION["mensagem-de-sucesso"] = "O participante ".$nome." compete na categoria adolescente";    
+    header("location: index.php");
 }
 else {
-    echo "O participante ".$nome." compete na categoria adulto";
+    $_SESSION["mensagem-de-sucesso"] = "O participante ".$nome." compete na categoria adulto";    
+    header("location: index.php");;
 }
+
